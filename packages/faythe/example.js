f37997d54109cb3eebbff2dbcab873884a91db6d
Javi Santos <mail@javisantos.com>
2020-05-25 23:26:00 +0200

    README

--- packages/faythe/example.js
+++ packages/faythe/example.js
@@ -1,16 +1,3 @@
-# Faythe
-
-An easy crypto library to send messages using key encapsulation. A courier for Alice and Bob.
-
-## Installation
-
-```sh
-npm install faythe --save
-```
-
-## Usage
-
-```js
 const faythe = require('.').v1
 
 const Alice = faythe.generateKeyPair()
@@ -18,9 +5,3 @@ const Bob = faythe.generateKeyPair()
 const packed = faythe.packMessage('Hello world', [Bob.publicKey], Alice)
 const unpacked = faythe.unpackMessage(packed, Bob).toString()
 console.log(unpacked) // Hello world
-
-```
-
-## License
-
-MIT
